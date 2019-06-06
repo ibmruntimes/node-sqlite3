09f319c11020b642a2eaa4aa9e04e8a739035e81
Shuowang (Wayne) Zhang <shuowang.zhang@ibm.com>
2019-06-06 16:19:24 -0400

    Fix scripts so that module will build

--- deps/extract.py
+++ deps/extract.py
@@ -5,16 +5,18 @@ import platform
 
 tarball = os.path.abspath(sys.argv[1])
 dirname = os.path.abspath(sys.argv[2])
-if platform.system() == 'OS/390':
-  import tempfile
-  tempfile = tempfile.NamedTemporaryFile()
-  cmdtype = ("cd %s"
-             " && (iconv -f IBM-1047 -t ISO8859-1 %s | gunzip -c > %s)"
-             " && pax -ofrom=ISO8859-1,to=IBM-1047 -rf %s")
-  cmd = cmdtype % (dirname, tarball, tempfile.name, tempfile.name)
-  os.system(cmd)
-else:
-  tfile = tarfile.open(tarball, 'r:gz')
-  tfile.extractall(dirname)
+# if platform.system() == 'OS/390':
+#   import tempfile
+#   tempfile = tempfile.NamedTemporaryFile()
+#   cmdtype = ("cd %s"
+#              " && (iconv -f IBM-1047 -t ISO8859-1 %s | gunzip -c > %s)"
+#              " && pax -ofrom=ISO8859-1,to=IBM-1047 -rf %s")
+#   cmd = cmdtype % (dirname, tarball, tempfile.name, tempfile.name)
+#   os.system(cmd)
+# else:
+tfile = tarfile.open(tarball, 'r:gz')
+tfile.extractall(dirname)
+# if platform.system() == 'OS/390':
+os.system("chtag -R -tc 819 %s" % dirname)
   
 sys.exit(0)
